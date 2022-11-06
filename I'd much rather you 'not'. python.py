aaf323f0afb3eb1dc68bf2839ae8bf1718933276
paras98765 <117525209+paras98765@users.noreply.github.com>
2022-11-06 12:34:44 +0530

    Add files via upload

---
+++ I'd much rather you 'not'. python.py
@@ -0,0 +1,8 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("This is fun");
+This is fun
+>>> print("Yay! Printing");
+Yay! Printing
+>>> print("I'd much rather you 'not'.");
+I'd much rather you 'not'.
